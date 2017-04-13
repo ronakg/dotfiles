124280e098496f0f59f683cac41fb2652f6212e1
Ronak Gandhi <ronak.gandhi@ronakg.com>
2017-04-13 16:48:37 -0700

    more changes

--- environment.sh
+++ environment.sh
@@ -1,7 +1,8 @@
 # Setup environment
 # Common to both bash and zsh
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 
 # Check if a command exists
 command_exists () {
