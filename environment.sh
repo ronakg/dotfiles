6e589ccaec33a0b52e1bc2e793441b5d7bbc899f
Ronak Gandhi <rogandhi@cisco.com>
2019-08-08 16:00:32 -0700

    FZF colorscheme

--- environment.sh
+++ environment.sh
@@ -84,5 +84,32 @@ if command_exists fzf; then
   elif command_exists ag; then
     export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
   fi
-  export FZF_DEFAULT_OPTS='--reverse --color light'
+
+  _gen_fzf_default_opts() {
+      local color01='#2D3B4D'
+      local color00='#1F2430'
+      local color02='#517F8D'
+      local color03='#6C8B91'
+      local color04='#CBCCC6'
+      local color05='#a1a19a'
+      local color06='#e6e6dc'
+      local color07='#fafaf8'
+      local color08='#ff5a67'
+      local color09='#f08e48'
+      local color0A='#BAE67E'
+      local color0B='#7fc06e'
+      local color0C='#F28779'
+      local color0D='#FFCC66'
+      local color0E='#9a70a4'
+      local color0F='#5CCFE6'
+
+      export FZF_DEFAULT_OPTS=" --reverse
+      --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
+      --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
+      --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0F
+      "
+
+  }
+
+_gen_fzf_default_opts
 fi
